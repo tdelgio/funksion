4593a2e653b1cf7458db5bff6435da822e656513
tdelgio <tomas.dg@me.com>
2021-04-08 12:20:45 -0300

    Initial commit from gatsby: (https://github.com/tdelgio/tailwind-gatsby-v3-starter.git)

---
+++ src/components/layout.js
@@ -0,0 +1,44 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <div className="my-0 mx-auto max-w-2xl p-4">
+        <main>{children}</main>
+        <footer className="mt-4">
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
