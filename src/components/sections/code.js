68212c6665f8a0b77b6b9e28ace9752c1c143057
tdelgio <tomas.dg@me.com>
2021-05-07 07:04:41 -0600

    reduce scale code module

--- src/components/sections/code.js
+++ src/components/sections/code.js
@@ -40,7 +40,7 @@ const Code = props => {
             <br />
           </h1>
         </div>
-        <div className="flex flex-col 2xl:w-2/3 lg:flex-row items-center justify-center lg:items-start text-lg tracking-wider leading-6 font-work-reg pt-8 lg:pt-0 lg:space-x-20  2xl:transform 2xl:scale-150 2xl:-translate-x-5">
+        <div className="flex flex-col 2xl:w-2/3 lg:flex-row items-center justify-center lg:items-start text-lg tracking-wider leading-6 font-work-reg pt-8 lg:pt-0 lg:space-x-20  2xl:transform 2xl:scale-125 2xl:-translate-x-5">
           <div className="flex flex-col items-center text-center py-8 hover:bg-red-700">
             <p className="font-work-semi text-4xl lg:text-5xl py-1">100%</p>
             <p className="font-work-md">{props.data.lighthouse1.textBlack}</p>
