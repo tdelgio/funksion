f9181259b55130da8345cd2a433a33d114aa5848
tdelgio <tomas.dg@me.com>
2021-07-15 17:08:37 -0500

    fix backblur ?

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Dropdown = () => {
   return (
-    <div className="block h-screen backdrop-filter backdrop-blur-sm fixed w-screen pt-1 z-10">
+    <div className="h-screen backdrop-filter backdrop-blur-sm w-screen pt-1 z-10">
       <svg
         width="233"
         height="244"
