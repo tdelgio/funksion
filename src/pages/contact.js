c71cb86e752a1c5f8175ce9f057c005a365382bc
tdelgio <tomas.dg@me.com>
2021-08-04 13:46:00 -0500

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import Contact from "../components/sections/contact"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Contact" />
+
+    <Contact />
+  </Layout>
+)
+
+export default ContactPage
