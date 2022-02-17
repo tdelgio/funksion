eded0f84edb35e18ea9a8b7fc0a823c1c832e48b
tdelgio <tomas.dg@me.com>
2022-02-17 17:36:39 -0600

    change short name

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `funksion`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
